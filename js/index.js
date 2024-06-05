299f49e969f8d03aa4e6ebc94641af93c358c2e6
Tom Greis <62010225+tomgreis@users.noreply.github.com>
2024-06-05 08:19:19 +0200

    add darkmode to profile page

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
-const mainElement = document.querySelector('[data-js="main"]');
+const bodyElement = document.querySelector('[data-js="body"]');
 const buttonDarkmode = document.querySelector('[data-js="button-darkmode"]');
 
-// console.log(mainElement);
+// console.log(bodyElement);
 // console.log(buttonDarkmode);
 
 buttonDarkmode.addEventListener("click", () => {
-  mainElement.classList.toggle("body--dark");
+  bodyElement.classList.toggle("body--dark");
 });
